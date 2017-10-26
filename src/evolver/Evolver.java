ccaa9c019b191630007d0e07bb6ae527885237a3
mmarkle <malcolmmaude01@gmail.com>
2017-10-26 15:49:18 -0500

    read the changes

--- src/evolver/Evolver.java
+++ src/evolver/Evolver.java
@@ -5,15 +5,15 @@ import java.time.Instant;
 public class Evolver {
 	
 	static String target = "Hello, I am malcolm.";
-	static int popsize = 100;
+	static int popsize = 1000;
 	
 	public static void main(String[] args) throws InterruptedException {
 		
 		//SET UP POPULATIONS
 		Population population = new Population(popsize);
 		int bestscore = 0;
-		double mutationRate = 0.1;
-		double lastMutationRate = 0.05;
+		double mutationRate = 0.01;
+		double lastMutationRate = 0.1;
 		boolean lastInc = true;
 		
 		//SET UP STATS STUFF
@@ -23,12 +23,19 @@ public class Evolver {
 		float millisElapsed = 0;
 		float lastMillisElapsed;
 		
-		while (stopOnFalse) {
+		/*while (stopOnFalse) {
+			generation = 0;
+			population = new Population(popsize);
+			starttime = Instant.now();*/
 			//MAIN ALGORITHM LOOP
+		while(true) {
+			generation = 0;
+			population = new Population(popsize);
+			starttime = Instant.now();
 			do {
 				generation++;
 				population.breed();
-				population.mutate(0.2);
+				population.mutate(mutationRate);
 				//population.displayBest();
 			} while (population.bestScore() > 0);
 		
@@ -36,11 +43,33 @@ public class Evolver {
 			Instant stoptime = Instant.now();
 			lastMillisElapsed = millisElapsed;
 			millisElapsed = stoptime.minusMillis(starttime.toEpochMilli()).toEpochMilli();
-			System.out.println("DONE IN " + (millisElapsed / 1000) + "s: generation " + generation);
+			System.out.println("DONE IN " + (millisElapsed / 1000) + "s:\tgeneration " + generation + ":\tmutation rate is " + mutationRate * 100 + "%");
 			
 			if (mutationRate > lastMutationRate && millisElapsed < lastMillisElapsed) {
+				lastMutationRate = mutationRate;
+				mutationRate += 0.001;
+			} else if (mutationRate > lastMutationRate && millisElapsed > lastMillisElapsed) {
+				lastMutationRate = mutationRate;
+				mutationRate -= 0.001;
+			} else if (mutationRate < lastMutationRate && millisElapsed < lastMillisElapsed) {
+				lastMutationRate = mutationRate;
+				mutationRate -= 0.001;
+			} else {
+				lastMutationRate = mutationRate;
+				mutationRate += 0.001;
 			}
 		}
+			/*
+			if (mutationRate > lastMutationRate && millisElapsed < lastMillisElapsed) {
+				mutationRate += 0.005;
+			} else if (mutationRate > lastMutationRate && millisElapsed > lastMillisElapsed) {
+				mutationRate -= 0.005;
+			} else if (mutationRate < lastMutationRate && millisElapsed < lastMillisElapsed) {
+				mutationRate -= 0.005;
+			} else {
+				mutationRate += 0.005;
+			}
+		}*/
 	}
 	
 }
