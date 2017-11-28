0e059465e473d41b9fd6cf9e82307eb4e4a0d105
mmarkle <mmarkle@users.noreply.github.com>
2017-11-28 16:56:05 -0600

    started adding javadocs

--- src/evolver/Evolver.java
+++ src/evolver/Evolver.java
@@ -4,31 +4,24 @@ import java.time.Instant;
 
 public class Evolver {
 	
-	static String target = "Hello, I am malcolm.";
-	static int popsize = 1000;
+	static String target = "Hello, World!";
+	static int popsize = 5000000;
 	
 	public static void main(String[] args) throws InterruptedException {
 		
 		//SET UP POPULATIONS
 		Population population = new Population(popsize);
-		int bestscore = 0;
-		double mutationRate = 0.01;
-		double lastMutationRate = 0.1;
-		boolean lastInc = true;
+		double mutationRate = 0.03;
 		
 		//SET UP STATS STUFF
 		int generation = 0;
+		/**
+		 * the start time
+		 */
 		Instant starttime = Instant.now();
-		boolean stopOnFalse = true;
 		float millisElapsed = 0;
-		float lastMillisElapsed;
-		
-		/*while (stopOnFalse) {
-			generation = 0;
-			population = new Population(popsize);
-			starttime = Instant.now();*/
+		 
 			//MAIN ALGORITHM LOOP
-		while(true) {
 			generation = 0;
 			population = new Population(popsize);
 			starttime = Instant.now();
@@ -36,40 +29,13 @@ public class Evolver {
 				generation++;
 				population.breed();
 				population.mutate(mutationRate);
-				//population.displayBest();
+				population.displayBest(starttime);
 			} while (population.bestScore() > 0);
 		
 			//DISPLAY STATS STUFF
 			Instant stoptime = Instant.now();
-			lastMillisElapsed = millisElapsed;
 			millisElapsed = stoptime.minusMillis(starttime.toEpochMilli()).toEpochMilli();
 			System.out.println("DONE IN " + (millisElapsed / 1000) + "s:\tgeneration " + generation + ":\tmutation rate is " + mutationRate * 100 + "%");
-			
-			if (mutationRate > lastMutationRate && millisElapsed < lastMillisElapsed) {
-				lastMutationRate = mutationRate;
-				mutationRate += 0.001;
-			} else if (mutationRate > lastMutationRate && millisElapsed > lastMillisElapsed) {
-				lastMutationRate = mutationRate;
-				mutationRate -= 0.001;
-			} else if (mutationRate < lastMutationRate && millisElapsed < lastMillisElapsed) {
-				lastMutationRate = mutationRate;
-				mutationRate -= 0.001;
-			} else {
-				lastMutationRate = mutationRate;
-				mutationRate += 0.001;
-			}
-		}
-			/*
-			if (mutationRate > lastMutationRate && millisElapsed < lastMillisElapsed) {
-				mutationRate += 0.005;
-			} else if (mutationRate > lastMutationRate && millisElapsed > lastMillisElapsed) {
-				mutationRate -= 0.005;
-			} else if (mutationRate < lastMutationRate && millisElapsed < lastMillisElapsed) {
-				mutationRate -= 0.005;
-			} else {
-				mutationRate += 0.005;
-			}
-		}*/
 	}
 	
 }
--- src/evolver/Population.java
+++ src/evolver/Population.java
@@ -2,6 +2,7 @@ package evolver;
 
 import java.util.Arrays;
 import java.util.Random;
+import java.time.Instant;
 
 public class Population {
 	private Organism[] population;
@@ -17,6 +18,9 @@ public class Population {
 		sorter = new Sorter(Evolver.target);
 	}
 	
+	/**
+	 * Fills the bottom half of the population with the breedings of the top half.
+	 */
 	public void breed() {
 		Arrays.sort(population, sorter);
 		for (int i = 0; i < population.length / 2; i++) {
@@ -24,22 +28,50 @@ public class Population {
 		}
 	}
 	
+	/**
+	 * Displays the entire population. Why would you want to do this? I don't know.
+	 */
 	public void displayPop() {
 		for (int i = 0; i < population.length; i++) {
 			System.out.println(population[i].getDNA() + " : " + population[i].score());
 		}
 	}
 	
+	/**
+	 * Displays the best Organism in the population, along with its score.
+	 * 
+	 * @output "{DNA of best Organism} : {score of best Organism}"
+	 */
 	public void displayBest() {
 		Arrays.sort(population, sorter);
 		System.out.println(population[0].getDNA() + " : " + population[0].score());
 	}
 	
+	/**
+	 * Displays the best Organism in the population, along with its score.
+	 * 
+	 * @param starttime the starting time, as an Instant
+	 * 
+	 * @output "{DNA of best Organism} : {score of best Organism}	: {time elapsed}s"
+	 */
+	public void displayBest(Instant starttime) {
+		Arrays.sort(population, sorter);
+		System.out.println(population[0].getDNA() + " : " + population[0].score() + "\t: " + ((float)Instant.now().minusMillis(starttime.toEpochMilli()).toEpochMilli() / 1000) + "s");
+	}
+	
+	/**
+	 * @return the best score from the population
+	 */
 	public int bestScore() {
 		Arrays.sort(population, sorter);
 		return population[0].score();
 	}
 	
+	/**
+	 * mutates the population
+	 * 
+	 * @param rate the rate of mutation, as a decimal
+	 */
 	public void mutate(double rate) {
 		for (int i = 0; i < population.length; i++) {
 			population[i].mutate(rate);
